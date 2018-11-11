d7b41cccf34a848f8c9c3a2b01b7bc23fa074bec
maddyb99 <maddyb99@users.noreply.github.com>
2018-11-12 04:19:50 +0530

    original Mitali

--- Assets/Scripts/StackController.cs
+++ Assets/Scripts/StackController.cs
@@ -44,24 +44,11 @@ public class StackController : MonoBehaviour {
 	}
     private void splitTile()
     {
-        x = tileTransition - i;
-        if (top>=2)
-        {
-             x =  stack[top-1].transform.localScale.x- stack[top - 2].transform.localScale.x;
-        }
-        else if (top >= 1)
-        {
-            x =stack[top - 1].transform.localScale.x - stack[transform.childCount - 1].transform.localScale.x;
-        }
-        else if (top <=0)
-        {
-            x = stack[transform.childCount - 1].transform.localScale.x - stack[transform.childCount - 2].transform.localScale.x;
-        }
-        float sc = stack[top-1].transform.localScale.x - x;
-        stack[top-1].transform.localScale=new Vector3(sc, 1, 1);
-        float nx = stack[top - 1].transform.localPosition.x-(sc/2);
-        stack[top - 1].transform.localPosition = new Vector3(nx, stack[top - 1].transform.localPosition.y, stack[top - 1].transform.localPosition.z);
+        x = tileTransition-i;
 
+        stack[top - 1].transform.localScale = new Vector3(Mathf.Abs(x), 1, 1);
+
+       
 
 
     }
@@ -88,9 +75,8 @@ public class StackController : MonoBehaviour {
        
     {
         tileTransition += speed * Time.deltaTime;
-       // i = (float)(stack[top - 2].transform.position.x);
-
-        if ((top - 1) % 2 != 0 || top == transform.childCount)
+     
+      if ((top - 1) % 2 != 0 || top == transform.childCount)
             stack[top - 1].transform.localPosition = new Vector3(Mathf.Sin(tileTransition), k, 0);
      else
             stack[top - 1].transform.localPosition = new Vector3(0, k, Mathf.Sin(tileTransition));
