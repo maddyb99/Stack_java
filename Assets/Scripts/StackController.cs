16c62d1642fd2ee0f6430306edea2c27246a1e4f
MaddyB <madhav.179201109@muj.manipal.edu>
2018-12-15 20:52:33 +0530

    fgb

--- Assets/Scripts/StackController.cs
+++ Assets/Scripts/StackController.cs
@@ -1,25 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StackController : MonoBehaviour {
 
     private GameObject[] stack;
-    private TextMesh dispscore;
-    private int score=0,Flag=1,j=0, k,top;
-    private float X=0,Z=0,scx=5,scz=5,i,speed = 2f,tileTransition=0.0f;
+    private Rigidbody drop;
+    public Text dispscore;
+    private int score=0,Flag=1,j=0, top,k=10;
+    private float X=0,Z=0,scx=5,scz=5,speed = 2f,tileTransition=0.0f;
     public int topIndex;
     private bool gameFlag = true;
 	void Start () {
         stack = new GameObject[transform.childCount];
-        dispscore = new TextMesh();
         for (j = 0; j < transform.childCount; j++)
         {
         stack[j] = transform.GetChild(j).gameObject;
         }
 
-        k = (int)(stack[transform.childCount-1 ].transform.position.y);
-        i = (float)(stack[transform.childCount-2].transform.position.x);
+     //   k = (int)(stack[transform.childCount-1 ].transform.position.y);
+   //     i = (float)(stack[transform.childCount-2].transform.position.x);
         topIndex = 0;
         top = transform.childCount;
     }
@@ -27,6 +28,8 @@ public class StackController : MonoBehaviour {
 
 	void Update () {
 
+        if (!gameFlag && Input.GetKeyDown("r"))
+            Reset();
         if (gameFlag)
         {
             if (score != 0 && Flag == 1 && score % 10 == 0)
@@ -40,12 +43,15 @@ public class StackController : MonoBehaviour {
             if (Input.GetKeyDown("space"))
             {
                 splitTile();
-                spawnTile();
-                score++;
                 gameFlag = endCheck();
-                
-                transform.position = new Vector3(transform.position.x, transform.position.y -1, transform.position.z);
-                dispscore.text = string.Empty + score;
+                if (gameFlag)
+                {
+                    spawnTile();
+                    score++;
+                    transform.position = new Vector3(transform.position.x, transform.position.y - 1, transform.position.z);
+                    Debug.Log(score);
+                    dispscore.text = string.Empty + score;
+                }
             }
         }
         else
@@ -55,26 +61,44 @@ public class StackController : MonoBehaviour {
     private void splitTile()
     {
         float xdiff=0,zdiff=0;
-
-
-            xdiff = stack[top - 1].transform.localPosition.x - X;
-            zdiff = stack[top - 1].transform.localPosition.z - Z;
-
+        xdiff = stack[top - 1].transform.localPosition.x - X;
+        zdiff = stack[top - 1].transform.localPosition.z - Z;
         scx = stack[top - 1].transform.localScale.x - Mathf.Abs(xdiff);
         scz = stack[top - 1].transform.localScale.z - Mathf.Abs(zdiff);
+        if(scx<0||scz<0)
+            return;
         stack[top - 1].transform.localScale = new Vector3(scx, 1, scz);
         X = stack[top - 1].transform.localPosition.x-(xdiff / 2f);
         Z = stack[top - 1].transform.localPosition.z - (zdiff / 2f);
         stack[top - 1].transform.localPosition = new Vector3(X, stack[top - 1].transform.localPosition.y, Z);
+       // stack[top-1].
                      
     }
-
+    private void Reset()
+    {
+        score = 0;
+        Flag = 1;
+        scx = 5;
+        scz = 5;
+        topIndex = 0;
+        top = transform.childCount;
+        gameFlag = true;
+        tileTransition = 0.0f;
+        k = 10;
+        X = 0;
+        Z = 0;
+        speed = 2f;
+        for (j = 0; j < transform.childCount; j++)
+        {
+            stack[j].transform.localPosition = new Vector3(X, j, Z);
+        }
+        transform.position = new Vector3(0,0,0);
+    }
     private void spawnTile() {
         
         k++;
         stack[topIndex].transform.localPosition = new Vector3(X, k, Z);
         stack[topIndex].transform.localScale = new Vector3(scx, 1, scz);
-
         topIndex++;
         if (top == transform.childCount)
         {
@@ -104,7 +128,7 @@ public class StackController : MonoBehaviour {
     {
         if (scx <= 0.1 || scx <= 0.1)
             return false;
-        if (stack[top - 1].transform.localPosition.x >= (X+scx) || stack[top - 1].transform.localPosition.x <= (X - scx)|| stack[top - 1].transform.localPosition.z >= (X+scz)|| stack[top - 1].transform.localPosition.z <= (X - scz))
+        if (stack[top - 1].transform.localPosition.x >= (X + scx) || stack[top - 1].transform.localPosition.x <= (X - scx) || stack[top - 1].transform.localPosition.z >= (X + scz) || stack[top - 1].transform.localPosition.z <= (X - scz))
             return false;
         return true;
     }
